94215f0997cd53c9575825e26f0ac86484123ea7
Shaikh Mohd Ovais <sovais333@gmail.com>
2022-09-20 09:10:50 +0530

    done

---
+++ Arrays/E-MaximumSubarray.java
@@ -0,0 +1,33 @@
+
+//https://leetcode.com/problems/maximum-subarray/
+
+class Solution {
+    public int maxSubArray(int[] arr) {
+        //Create two variables : one will store sum of the current subarray and other will store maximum in the whole array
+
+        //Taking MIN_VALUE so that it gets upadted even when negative numbers are present in array
+        int maxSoFar = Integer.MIN_VALUE;
+        int maxEndingHere = 0;
+        
+        int length = arr.length;
+
+        for(int i = 0; i < length; i++) {
+            maxEndingHere += arr[i];
+            
+            //If sum of current subarray is greater then we update maxSoFar
+            if(maxEndingHere > maxSoFar) {
+                maxSoFar = maxEndingHere;
+            }
+
+            //If sum is negative then we change it to 0 and start again
+            if(maxEndingHere < 0) {
+                maxEndingHere = 0;
+            }
+        }
+
+        return maxSoFar;
+    }
+}
+
+//Time complexity : O(n) because we will be iterating over complete array for one time
+//Space complexity : O(1) because no extra space is used
--- Arrays/E-moveZeroes.java
+++ Arrays/E-moveZeroes.java
@@ -10,12 +10,14 @@ class Solution {
         int pointer = 0;
         for(int i = 0; i < nums.length; i++) {
             if(nums[i] != 0) {
-                nums[pointer++] = nums[i];
+                nums[pointer] = nums[i];
+                pointer++;
             }
         }
         
         while(pointer < nums.length) {
-            nums[pointer++] = 0;
+            nums[pointer] = 0;
+            pointer++;
         }
     }
 }
